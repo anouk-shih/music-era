d11afcb9dd842684a8bad7faf25655c5d6e141be
Anouk Shih <Anouk.Shih@osp.com.tw>
2025-01-17 09:26:15 +0800

    Refactor StepSlogan component to use single quotes for imports and enhance background styling with cover, center positioning, and no-repeat options.

--- src/components/StepSlogan.tsx
+++ src/components/StepSlogan.tsx
@@ -1,8 +1,8 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from 'react';
 
-import SloganImage from "../assets/slogan.png";
-import SloganAudio from "../assets/slogan.wav";
-import { KEY_NEXT } from "../constant";
+import SloganImage from '../assets/slogan.png';
+import SloganAudio from '../assets/slogan.wav';
+import { KEY_NEXT } from '../constant';
 
 const StepSlogan = ({ handleNext }: { handleNext: (e: KeyboardEvent) => void }) => {
   const [step, setStep] = useState(0);
@@ -41,7 +41,12 @@ const StepSlogan = ({ handleNext }: { handleNext: (e: KeyboardEvent) => void }) 
   return (
     <div
       className="relative grid w-full h-full backdrop-blur-md place-items-center"
-      style={{ backgroundImage: `url(${SloganImage})`, backgroundSize: "contain" }}>
+      style={{
+        backgroundImage: `url(${SloganImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}>
       <p className="absolute text-4xl text-center text-white micro-5-regular bottom-10">
         {"<-"}
         <span className="ml-20 text-white micro-5-regular">{"->"}</span>
